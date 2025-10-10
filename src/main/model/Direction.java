cfb97370481c1043aea4e9cb3f8dead20a2a06e4
Natalie Skupchenko <nskup67@students.cs.ubc.ca>
2025-10-09 22:05:48 -0700

    Direction enum

---
+++ src/main/model/Direction.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Represents the possible directions that an Elevator can be moving.
+ * UP: Elevator is moving upward.
+ * DOWN: Elevator is moving downward.
+ * IDLE: Elevator is not moving.
+ */
+
+public enum Direction {
+    UP,
+    DOWN,
+    IDLE
+}
