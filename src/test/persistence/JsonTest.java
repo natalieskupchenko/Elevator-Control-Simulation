99b490ed8fb67e3bd8e45ad5b2b0d11f0db6c122
Natalie Skupchenko <nskup67@students.cs.ubc.ca>
2025-10-24 13:47:36 -0700

    Added @ExcludeFromJacocoGeneratedReport

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,11 +1,14 @@
 package persistence;
 
 import model.Elevator;
+import model.ExcludeFromJacocoGeneratedReport;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Code referenced and adapted from CPSC 210 JsonSerializationDemo
 // URL: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 
+@ExcludeFromJacocoGeneratedReport
 public class JsonTest {
     protected void checkElevator(int floors, int currentFloor, int numRequests, Elevator e) {
         assertEquals(floors, e.getFloorsInBuilding());
